ff8c99f3a02db573f08fd892356c672b7665232d
parthmern <parthmern@gmail.com>
2024-05-16 08:57:19 -0400

    readme added

--- 10_Week-10/week-13-sql/src/insert-data.ts
+++ 10_Week-10/week-13-sql/src/insert-data.ts
@@ -5,7 +5,7 @@ async function createEntries() {
     const insertUserText = 'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id';
     const userValues = ['john.do11e@gmail2.com', 'hashed_password_here'];
 
-    let response = await client.query(insertUserText, userValues);
+    let response = await client.query(insertUserText, userValues); 
     const insertTodoText = 'INSERT INTO todos (title, description, user_id, done) VALUES ($1, $2, $3, $4) RETURNING id';
     const todoValues = ['Buy groceries', 'Milk, bread, and eggs', response.rows[0].id, false];
     await client.query(insertTodoText, todoValues);
--- 10_Week-10/week-13-sql/src/utils.ts
+++ 10_Week-10/week-13-sql/src/utils.ts
@@ -1,5 +1,6 @@
 import { Client } from 'pg';
 
+// connection with db
 export async function getClient() {
     const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
     await client.connect();
